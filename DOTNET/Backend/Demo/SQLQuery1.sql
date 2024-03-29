ef45effc5d7086f2433391602c885459f499b3d8
Proire <prasadahire4501@gmail.com>
2024-03-29 21:48:40 +0530

    DotNet Version first Backend and FrontEnd

---
+++ DOTNET/Backend/Demo/SQLQuery1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Passes] (
+    [PassengerId]     INT            IDENTITY (1, 1) NOT NULL,
+    [PassengerName]   NVARCHAR (MAX) NULL,
+    [BirthDate]       NVARCHAR (MAX) NULL,
+    [PassengerType]   NVARCHAR (MAX) NULL,
+    [PassengerAmount] INT            NOT NULL,
+    [custId]       INT            NOT NULL,
+    CONSTRAINT [PK_Passes] PRIMARY KEY CLUSTERED ([PassengerId] ASC),
+);
+
+
+
+
